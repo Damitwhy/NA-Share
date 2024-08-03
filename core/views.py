c076691c9d2ad2cf2ec338bd0356731dc590b988
Kenneth Cox <damitwhy@gmail.com>
2024-08-03 03:01:53 +0000

    edited home.html

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Post
+# from .models import Post
 
 # Create your views here.
 def home(request):
