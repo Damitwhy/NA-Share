608002a7fd0bcee7ca77027b9a24c378f7969d75
Kenneth Cox <damitwhy@gmail.com>
2024-08-06 11:34:52 +0000

    added favicon

--- core/admin.py
+++ core/admin.py
@@ -3,15 +3,18 @@ from .models import Share, Comment, User, Message
 from django_summernote.admin import SummernoteModelAdmin
 from django.contrib import messages
 
-# Register the Share model with the admin site
-admin.site.register(Share, SummernoteModelAdmin)
-admin.site.register(Comment)
-admin.site.register(User)
-admin.site.register(Message)
-
 def show_messages(modeladmin, request, queryset):
     for obj in queryset:
         messages.info(request, f"Message for {obj}")
 show_messages.short_description = "Show Messages"
 
+def is_admin_or_superuser(user):
+    return user.is_authenticated and (user.is_superuser or user.is_staff)
+
+# Register the Share model with the admin site
+admin.site.register(Share, SummernoteModelAdmin)
+admin.site.register(Comment)
+admin.site.register(User)
+admin.site.register(Message)
+# Define an admin action to show messages
 admin.site.add_action(show_messages)
