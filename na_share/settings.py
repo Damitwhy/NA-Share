c1f0659b070cc84edcfc46cf0ce9634d629faba8
Kenneth Cox <damitwhy@gmail.com>
2024-08-10 14:21:55 +0000

    edited CSS to add style to django-messages

--- na_share/settings.py
+++ na_share/settings.py
@@ -29,16 +29,12 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','.ws.codeinstitute-ide.net','.herokuapp.com']
 
 # Specify the custom user model
 AUTH_USER_MODEL = 'core.User'
-
-
 
 # Application definition
 
