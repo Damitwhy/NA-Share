38e5e37ba0ea8021a284494e95f6a47a5aca79fb
Kenneth Cox <damitwhy@gmail.com>
2024-08-13 01:56:59 +0000

    landscape settings to false

--- na_share/settings.py
+++ na_share/settings.py
@@ -31,7 +31,7 @@ TIME_ZONE = 'GB'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','.ws.codeinstitute-ide.net','.herokuapp.com']
 
